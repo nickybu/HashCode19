0157d55c03a39321ea77cc4d719aaf679594db54
Jack Davison <jlogandavison@gmail.com>
2019-02-28 20:13:52 +0000

    Optimizing random solution generator

--- random_solution.py
+++ random_solution.py
@@ -1,8 +1,19 @@
 from random import shuffle
+import signal
 import sys
 
-from hashcode.parsing import parse_dataset, print_solution
+from hashcode.parsing import *
 from hashcode.model import *
+
+max_solution = None
+max_score = None
+
+def exit_handler(sig, frame):
+    global max_solution
+    print_solution(solution)
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, exit_handler)
 
 dataset = 'a'
 if len(sys.argv) == 2:
@@ -22,7 +33,7 @@ horizontal_images = filter(lambda x: x.orientation == 'horizontal', images)
 vertical_image = filter(lambda x: x.orientation == 'vertical', images)
 
 def rand_vertical_slides(images):
-    vertical_images = filter(lambda x: x.orientation == 'vertical', images)
+    vertical_images = list(filter(lambda x: x.orientation == 'vertical', images))
     shuffle(vertical_images)
     
     if len(vertical_images) % 2 != 0:
@@ -32,8 +43,16 @@ def rand_vertical_slides(images):
     return [VerticalSlide(left, right) for left, right in grouped]
 
 
-horizontal_slides = [HorizontalSlide(i) for i in horizontal_images]
-vertical_slides = rand_vertical_slides(images)
+while True:
+    horizontal_slides = [HorizontalSlide(i) for i in horizontal_images]
+    vertical_slides = rand_vertical_slides(images)
 
-print_solution(horizontal_slides + vertical_slides)
+    solution = horizontal_slides + vertical_slides
+    score = compute_score(solution)
+
+    if max_score == None or score > max_score:
+        max_solution = solution
+        max_score = score
+        print >> sys.stderr, score
+
 
