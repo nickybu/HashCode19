c10e5dac1b3075e3965c964f26783b3e535ca3ee
Jack Davison <jlogandavison@gmail.com>
2019-02-28 18:26:57 +0000

    add IDs to parsing

--- hashcode/parsing.py
+++ hashcode/parsing.py
@@ -3,14 +3,14 @@ from .model import Image
 def parse_dataset(filename):
     with open(filename) as f:
         f.readline() # Discard the first line
-        images = [_parse_image(line) for line in f.readlines()]
+        images = [_parse_image(_id, line) for _id, line in enumerate(f.readlines())]
 
     return images
 
-def _parse_image(line):
+def _parse_image(_id, line):
     tokens = line.split()
 
     orientation = tokens[0]
     images = tokens[2:]
-    return Image(orientation, images)
+    return Image(_id, orientation, images)
 
